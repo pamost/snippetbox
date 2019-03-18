e7651fdc86416dc617db4798ff0c6f5c2a0511fe
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2019-03-18 10:13:51 +0600

    http.Error Shortcut

--- main.go
+++ main.go
@@ -36,8 +36,9 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		// response header map. The first parameter is the header name, and
 		// the second parameter is the header value.
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
